b43795a9725d125df391ee53f6cf9f7604469840
anupamGak <anupamgak07@gmail.com>
2015-06-02 10:31:29 +0530

    Code to remove links from htmlsoup

--- wikiplot.py
+++ wikiplot.py
@@ -1,9 +1,13 @@
 from lxml import html
 import requests
+import re
 
-schpage = html.parse("http://www.wikipedia.org").getroot()
+reAnchor = re.compile("<a.+?>")
+reTable = re.compile("<table[\w\W]+<\/table>")
 
 search = raw_input("Search :")
+
+schpage = html.parse("http://www.wikipedia.org").getroot()
 
 schpage.forms[0].fields['search'] = search
 
@@ -11,11 +15,14 @@ wiki = html.parse(html.submit_form(schpage.forms[0])).getroot()
 
 renodes = wiki.xpath("//h2[span='Plot']/following-sibling::*")
 
-htmlfrag = ""
+soup = ""
 for renode in renodes:
 	if renode.tag != 'h2':
-		htmlfrag += html.tostring(renode)
+		soup += html.tostring(renode)
 	else:
 		break
 
+htmlfrag = re.sub(reAnchor, "", soup)
+htmlfrag = re.sub("</a>", "", htmlfrag)
+htmlfrag = re.sub(reTable, "", htmlfrag)
 print htmlfrag
